eae03acc493ca0da430afbc7e55d69e510cc4c03
mafiaf <barisfirat@hotmail.com>
2025-06-13 15:47:37 +0200

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './templates/**/*.html',
+    './templates_en/**/*.html',
+    './**/*.py'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Manrope', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
